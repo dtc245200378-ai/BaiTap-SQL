19b8abf3920f3fefa505ce6992ac39afbdfb6c02
Khaong Tuan Hung <DTC245200378@Ictu.edu.vn>
2026-09-23 08:55:46 +0700

    Add practice: advanced select queries and update operation on QuanLySinhVien database

---
+++ QuanLySinhVien_QueryPractice.sql
@@ -0,0 +1,30 @@
+USE QuanLySinhVien;
+
+-- 1. Hiển thị tất cả các sinh viên có tên bắt đầu bằng ký tự 'h'
+SELECT * 
+FROM Student 
+WHERE StudentName LIKE 'h%';
+
+-- 2. Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12
+SELECT * 
+FROM Class 
+WHERE MONTH(StartDate) = 12;
+
+-- 3. Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5
+SELECT * 
+FROM Subject 
+WHERE Credit BETWEEN 3 AND 5;
+
+-- 4. Thay đổi mã lớp (ClassID) của sinh viên có tên 'Hung' thành 2
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Student 
+SET ClassID = 2 
+WHERE StudentName = 'Hung';
+SET SQL_SAFE_UPDATES = 1;
+
+-- 5. Hiển thị StudentName, SubName, Mark (Sắp xếp Mark giảm dần, nếu trùng sắp xếp tên tăng dần)
+SELECT S.StudentName, Sub.SubName, M.Mark
+FROM Student S
+JOIN Mark M ON S.StudentId = M.StudentId
+JOIN Subject Sub ON M.SubId = Sub.SubId
+ORDER BY M.Mark DESC, S.StudentName ASC;
